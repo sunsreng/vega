33a58018c651cfb7de1c65a1854c3eef48b8808c
sun sreng <sreng@infinity.com.kh>
2017-09-29 10:35:12 +0700

    commit

---
+++ Vega.Api/Vega.Api/Controllers/VehiclesController.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+using Vega.Api.Controllers.Resources;
+using Vega.Api.Models;
+using Vega.Api.Persistence;
+
+namespace Vega.Api.Controllers
+{
+    [Route("/api/vehicles")]
+    public class VehiclesController : Controller
+    {
+        private readonly IMapper mapper;
+        private readonly VegaDbContext context;
+
+        public VehiclesController(IMapper mapper, VegaDbContext context)
+        {
+            this.mapper = mapper;
+            this.context = context;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateVehicle([FromBody] VehicleResource vehicleResource)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            //var model = await context.Models.FindAsync(vehicleResource.ModelId);
+            //if (model == null)
+            //{
+            //    ModelState.AddModelError("ModelId", "Invalid model id");
+            //    return BadRequest(ModelState);
+            //}
+
+            var vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleResource);
+            vehicle.LastUpdate = DateTime.Now;
+            context.Vehicles.Add(vehicle);
+            await context.SaveChangesAsync();
+            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
+            return Ok(result);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateVehicle(int id, [FromBody] VehicleResource vehicleResource)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            var vehicle = await context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
+            mapper.Map<VehicleResource, Vehicle>(vehicleResource, vehicle);
+            vehicle.LastUpdate = DateTime.Now;
+
+            await context.SaveChangesAsync();
+
+            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
+            return Ok(result);
+        }
+    }
+}
--- Vega.Api/Vega.Api/Persistence/VegaDbContext.cs
+++ Vega.Api/Vega.Api/Persistence/VegaDbContext.cs
@@ -5,6 +5,8 @@ namespace Vega.Api.Persistence
 {
     public class VegaDbContext : DbContext
     {
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<Model> Models { get; set; }
         public DbSet<Make> Makes { get; set; }
         public DbSet<Feature> Features { get; set; }
         public VegaDbContext(DbContextOptions<VegaDbContext> options) : base(options)
